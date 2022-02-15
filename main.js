595d3a6b383e683283014c4ccdcf7633cc4440f9
moulik13238l <86113949+moulik13238l@users.noreply.github.com>
2022-02-15 19:40:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song1 ="";
+song2 ="";
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    snog2 = loadSound("Eyy Bidda Ye Mera Adda - Pushpa.mp3");
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function draw (){
+    image(video, 0, 0, 600, 500);
+}
+
+function play () {
+    song.play()
+}
